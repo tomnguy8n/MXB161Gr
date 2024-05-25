58680d7c0387e65476feca6c520947ebb7370c1e
tomnguy8n <126861372+tomnguy8n@users.noreply.github.com>
2024-05-26 00:09:29 +1000

    Update infection_process.m
    updates to have reinfection probability and euclidean distance

--- Tom_prototype/Modular_version/infection_process.m
+++ Tom_prototype/Modular_version/infection_process.m
@@ -1,4 +1,4 @@
-function [newStates, newInfectionTime] = infection_process(agentStates, agentPositions, gridSize, p0, infectionRadius, neighborhoodType, infectionDuration)
+function [newStates, newInfectionTime] = infection_process(agentStates, agentPositions, gridSize, p0, infectionRadius, neighborhoodType, infectionDuration, agentRecoveries, reinfectionProbBase)
     % Initialize new states and infection time arrays
     newStates = agentStates;
     newInfectionTime = zeros(size(agentStates));
@@ -8,9 +8,16 @@ function [newStates, newInfectionTime] = infection_process(agentStates, agentPos
         if newStates(i) == 0 % Only consider susceptible agents
             nSurrounding = count_infected_neighbors(agentPositions(i, :), agentPositions, agentStates, gridSize, infectionRadius, neighborhoodType);
             
-            % Probability of spreading infection
+            % Calculate reinfection probability based on recoveries and number of infected neighbors
             if nSurrounding > 0
-                infectionProbability = 1 - (1 - p0)^nSurrounding;
+                if agentRecoveries(i) > 0
+                    reinfectionProbability = (reinfectionProbBase) ^ agentRecoveries(i) * p0;
+                else
+                    reinfectionProbability = p0;
+                end
+                
+                % Calculate the final infection probability considering all infected neighbors
+                infectionProbability = 1 - (1 - reinfectionProbability) ^ nSurrounding;
                 if rand <= infectionProbability
                     newStates(i) = 1;
                     newInfectionTime(i) = infectionDuration;
@@ -22,28 +29,39 @@ end
 
 function nSurrounding = count_infected_neighbors(currentPos, agentPositions, agentStates, gridSize, infectionRadius, neighborhoodType)
     nSurrounding = 0;
-    for surrounding_x = -infectionRadius:infectionRadius
-        for surrounding_y = -infectionRadius:infectionRadius
-            if strcmp(neighborhoodType, 'Moore')
-                if surrounding_x == 0 && surrounding_y == 0
-                    continue;
+    for i = 1:size(agentPositions, 1)
+        if strcmp(neighborhoodType, 'Moore') || strcmp(neighborhoodType, 'Neumann')
+            % Use the existing method for Moore and Neumann neighborhoods
+            for surrounding_x = -infectionRadius:infectionRadius
+                for surrounding_y = -infectionRadius:infectionRadius
+                    if strcmp(neighborhoodType, 'Moore')
+                        if surrounding_x == 0 && surrounding_y == 0
+                            continue;
+                        end
+                    elseif strcmp(neighborhoodType, 'Neumann')
+                        if abs(surrounding_x) + abs(surrounding_y) > infectionRadius || (surrounding_x == 0 && surrounding_y == 0)
+                            continue;
+                        end
+                    end
+                    check_x = currentPos(1) + surrounding_x;
+                    check_y = currentPos(2) + surrounding_y;
+                    if check_x >= 1 && check_x <= gridSize && check_y >= 1 && check_y <= gridSize
+                        % Find if there's an infected agent at this position
+                        neighborIndex = find(agentPositions(:,1) == check_x & agentPositions(:,2) == check_y, 1);
+                        if ~isempty(neighborIndex) && agentStates(neighborIndex) == 1
+                            nSurrounding = nSurrounding + 1;
+                        end
+                    end
                 end
-            elseif strcmp(neighborhoodType, 'Neumann')
-                if abs(surrounding_x) + abs(surrounding_y) > infectionRadius || (surrounding_x == 0 && surrounding_y == 0)
-                    continue;
-                end
-            else
-                error('Invalid neighborhood type');
             end
-            check_x = currentPos(1) + surrounding_x;
-            check_y = currentPos(2) + surrounding_y;
-            if check_x >= 1 && check_x <= gridSize && check_y >= 1 && check_y <= gridSize
-                % Find if there's an infected agent at this position
-                neighborIndex = find(agentPositions(:,1) == check_x & agentPositions(:,2) == check_y, 1);
-                if ~isempty(neighborIndex) && agentStates(neighborIndex) == 1
-                    nSurrounding = nSurrounding + 1;
-                end
+        elseif strcmp(neighborhoodType, 'Euclidean')
+            % Use Euclidean distance for the new neighborhood type
+            distance = sqrt((currentPos(1) - agentPositions(i, 1))^2 + (currentPos(2) - agentPositions(i, 2))^2);
+            if distance > 0 && distance <= infectionRadius && agentStates(i) == 1
+                nSurrounding = nSurrounding + 1;
             end
+        else
+            error('Invalid neighborhood type');
         end
     end
 end
