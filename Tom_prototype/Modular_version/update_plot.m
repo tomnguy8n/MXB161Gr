7aa14ce38b4b83211c7ee6b624a4bb4bd45ea612
tomnguy8n <126861372+tomnguy8n@users.noreply.github.com>
2024-05-26 17:52:42 +1000

    Update update_plot.m

--- Tom_prototype/Modular_version/update_plot.m
+++ Tom_prototype/Modular_version/update_plot.m
@@ -1,7 +1,8 @@
-function update_plot(susceptible_plot, infected_plot, recovered_plot, susceptible_counts, infected_counts, recovered_counts)
+function update_plot(susceptible_plot, infected_plot, recovered_plot, dead_plot, susceptible_counts, infected_counts, recovered_counts, dead_counts, step)
     % Update the real-time plot
-    set(susceptible_plot, 'YData', susceptible_counts);
-    set(infected_plot, 'YData', infected_counts);
-    set(recovered_plot, 'YData', recovered_counts);
+    set(susceptible_plot, 'XData', 1:step, 'YData', susceptible_counts(1:step));
+    set(infected_plot, 'XData', 1:step, 'YData', infected_counts(1:step));
+    set(recovered_plot, 'XData', 1:step, 'YData', recovered_counts(1:step));
+    set(dead_plot, 'XData', 1:step, 'YData', dead_counts(1:step));
     drawnow;
 end
