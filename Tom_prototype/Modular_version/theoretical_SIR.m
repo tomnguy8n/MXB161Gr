661f2c3d4fe3ad73f79914b3da081d6b1038f0d1
Arzuz1 <arzuzrog.major@gmail.com>
2024-05-28 13:54:03 +1000

    Add files via upload
    Theoretical SIR model to compare with experimental (Matlab simulation).

---
+++ Tom_prototype/Modular_version/theoretical_SIR.m
@@ -0,0 +1,17 @@
+function [S,I,R] = theoretical_SIR(beta,gamma,delta,N,I0,T,dt)
+    % if delta = 0 we assume a model without immunity loss
+    S = zeros(1,T/dt);
+    S(1) = N;
+    I = zeros(1,T/dt);
+    I(1) = I0;
+    R = zeros(1,T/dt);
+    for tt = 1:(T/dt)-1
+        % Equations of the model
+        dS = (-beta*I(tt)*S(tt) + delta*R(tt)) * dt;
+        dI = (beta*I(tt)*S(tt) - gamma*I(tt)) * dt;
+        dR = (gamma*I(tt) - delta*R(tt)) * dt;
+        S(tt+1) = S(tt) + dS;
+        I(tt+1) = I(tt) + dI;
+        R(tt+1) = R(tt) + dR;
+    end
+end
