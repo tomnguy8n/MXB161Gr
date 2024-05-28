d81542bb6bfe6e6561293904c5c639ceaa921f52
Arzuz1 <arzuzrog.major@gmail.com>
2024-05-28 14:10:46 +1000

    Update theoretical_SIR.m
    Added a time vector for graphing the results.

--- Tom_prototype/Modular_version/theoretical_SIR.m
+++ Tom_prototype/Modular_version/theoretical_SIR.m
@@ -1,10 +1,11 @@
-function [S,I,R] = theoretical_SIR(beta,gamma,delta,N,I0,T,dt)
+function [S,I,R,time] = theoretical_SIR(beta,gamma,delta,N,I0,T,dt)
     % if delta = 0 we assume a model without immunity loss
     S = zeros(1,T/dt);
     S(1) = N;
     I = zeros(1,T/dt);
     I(1) = I0;
     R = zeros(1,T/dt);
+    time = 1:(T/dt);
     for tt = 1:(T/dt)-1
         % Equations of the model
         dS = (-beta*I(tt)*S(tt) + delta*R(tt)) * dt;
